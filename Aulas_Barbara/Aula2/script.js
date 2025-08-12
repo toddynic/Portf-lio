0b83a554727d26c812535e8e190b1849c5c02d07
Isac Castro <53198685+toddynic@users.noreply.github.com>
2025-08-12 11:05:17 -0300

    Add files via upload

---
+++ Aulas_Barbara/Aula2/script.js
@@ -0,0 +1,14 @@
+function clicou() {
+    const mens = document.getElementById("mensagem");
+    const butao = document.getElementById("butao");
+    const titulo = document.getElementById("titulo");
+    const titu = document.getElementById("titu");
+
+    mens.innerHTML = "Você ganhou um encontro com o Bolsonaro!!!!!";
+    butao.innerHTML = "Sortudo!!";
+    titulo.innerHTML = "NADA AQUI TAMBEM";
+    titu.innerHTML = "PAAARABEEENS!!!!!!";
+
+    
+    mens.style.color = "red";
+}
